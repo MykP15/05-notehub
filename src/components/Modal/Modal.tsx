abac81e023546be7efbc77094528f50d92c9c42e
Mykhailo Pyroh <mykhailopk@gmail.com>
2026-01-06 20:28:30 +0100

    add backdrop and esc

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { createPortal } from "react-dom"
 import css from "./Modal.module.css"
 import NoteForm from "../NoteForrm/NoteForm"
+import { useEffect } from "react"
 
 interface ModalProps {
   onClose: () => void
@@ -8,13 +9,34 @@ interface ModalProps {
 
 
 const modalRoot = document.getElementById("modal-root")
-function Modal({onClose}: ModalProps) {
+function Modal({ onClose }: ModalProps) {
+  
+useEffect(() => {
+const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") onClose();
+    };
+
+    document.body.style.overflow = "hidden";
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      document.body.style.overflow = "";
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, [onClose])
+
+  const handleBackdrop = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) onClose();
+  };
+
+
     return createPortal(
         <>
         <div
   className={css.backdrop}
   role="dialog"
-  aria-modal="true"
+          aria-modal="true"
+          onClick={handleBackdrop}
 >
   <div className={css.modal}>
             <NoteForm onCancel={onClose} />
